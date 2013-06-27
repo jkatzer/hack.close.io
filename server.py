0cccc28c85501bd63e8465de4272cd80801f444f
Thomas Steinacher <tom@eggdrop.ch>
2013-06-27 13:02:30 -0700

    Server: Use public_html dir by default. Fix links without extension. Fix reusing of socket.

--- server.py
+++ server.py
@@ -2,12 +2,19 @@ import os
 import SimpleHTTPServer
 import SocketServer
 
-os.chdir('.generated')
+os.chdir('../public_html')
 PORT = 8080 
 HOSTNAME = "localhost"
 
-httpd = SocketServer.TCPServer((HOSTNAME, PORT), 
-                                SimpleHTTPServer.SimpleHTTPRequestHandler)
+class BlogHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def translate_path(self, path):
+        name, ext = os.path.splitext(path)
+        if name.startswith('/posts/') and not ext:
+            path += '.html'
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(self, path)
+
+SocketServer.TCPServer.allow_reuse_address = True
+httpd = SocketServer.TCPServer((HOSTNAME, PORT), BlogHandler)
 
 print("serving on http://%s:%d" % (HOSTNAME, PORT))
 httpd.serve_forever()
